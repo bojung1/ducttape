a1b903f2262bc45eb4c1357ddf30047938c3960e
Administra-manual <admin@sti5.com>
2023-10-17 01:08:35 -0700

    Updated messaging

--- ping.sh
+++ ping.sh
@@ -11,7 +11,9 @@ while true; do
 echo "" >> ${LOGPATH}; 
 sleep 30;
 
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" >> ${LOGPATH};
 echo $(date "+%D %T") >> ${LOGPATH}; 
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" >> ${LOGPATH};
 ping -q -c5 ${CHIA_HOST} >> ${LOGPATH} 2>&1; 
 HOST_CONDITION=$?; 
 
@@ -21,14 +23,19 @@ CHIA_CONDITION=$?;
 
 if [[ $HOST_CONDITION != 0 ]]; then 
   spd-say "PC chia host box down" -p 40 -t female1 -r -50;
-  echo "Chia host box ping down" >> ${LOGPATH}; 
+  echo "*******************************************" >> ${LOGPATH};
+  echo "****Chia HOST BOX ping DOWN****" >> ${LOGPATH}; 
+  echo "********************************************" >> ${LOGPATH};
 fi;
 
 sleep 1; 
 
 if [[ $CHIA_CONDITION != 0 ]]; then
   spd-say "Chia client tcp down"  -t female2 -r -50;
-  echo "Chia client box tcp port down" >> ${LOGPATH}; 
+  echo "#######################################################" >> ${LOGPATH};
+  echo "##### Chia CLIENT BOX TCP port DOWN #########" >> ${LOGPATH}; 
+  echo "#######################################################" >> ${LOGPATH};
+
 fi;
 
 echo "" >> ${LOGPATH}; 
